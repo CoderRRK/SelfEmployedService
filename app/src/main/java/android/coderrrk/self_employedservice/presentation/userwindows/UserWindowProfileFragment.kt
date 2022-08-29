9ce9c220a28056edcc71f5f2d5baa75e655d4625
CoderRRK <rrkomlev@gmai.come>
2022-08-29 23:05:34 +0300

    Add PageAdapter

---
+++ app/src/main/java/android/coderrrk/self_employedservice/presentation/userwindows/UserWindowProfileFragment.kt
@@ -0,0 +1,7 @@
+package android.coderrrk.self_employedservice.presentation.userwindows
+
+import android.coderrrk.self_employedservice.R
+import androidx.fragment.app.Fragment
+
+class UserWindowProfileFragment: Fragment(R.layout.user_window_profile) {
+}
---
+++ app/src/main/java/android/coderrrk/self_employedservice/presentation/userwindows/UserWindowRootFragment.kt
@@ -0,0 +1,47 @@
+package android.coderrrk.self_employedservice.presentation.userwindows
+
+import android.coderrrk.self_employedservice.R
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager.widget.PagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.user_window_root_fragment.view.*
+
+class UserWindowRootFragment: Fragment() {
+
+    private var ctx: Context?= null
+
+    override fun onAttach(context: Context){
+        super.onAttach(context)
+        ctx = context
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.user_window_root_fragment, container, false)
+        view.tab_layout.tabIconTint = null
+        view.view_pager.adapter = UserWindowPageAdapter(ctx as FragmentActivity)
+
+        TabLayoutMediator(view.tab_layout, view.view_pager){
+                tab, pos ->
+            when(pos){
+                0 -> {
+                    tab.setIcon(R.drawable.ic_baseline_home_24)
+                }
+                1 -> {
+                    tab.setIcon(R.drawable.ic_baseline_person_24)
+                }
+            }
+        }.attach()
+        return view
+    }
+
+
+}
